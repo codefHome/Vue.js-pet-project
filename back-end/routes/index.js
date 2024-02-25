7335612526eed342e2a960c0929bdc0e8f726101
bedada <bbekele@excellerentsolutions.com>
2024-02-25 15:49:36 +0300

    updated

--- back-end/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const { sendMail } = require('../src/controller/sendMail');
-const { deleteDocument } = require('../src/controller/deleteDocument');
-var router = express.Router();
-
-/* GET home page. */
-router.post('/sendMail', sendMail);
-router.delete('/deleteAll',deleteDocument)
-module.exports = router;
